7601d9b69ca335b686f4fe90304c99fd167840bd
abhinav <abhinavsinghkkr@gmail.com>
2024-02-22 08:13:34 +0530

    Add todo status components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layouts/header/header.component';
+import { TodoStatusComponent } from './todo-status/todo-status.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TodoStatusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/todo-status/todo-status.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-status.component.html',
+  styleUrl: './todo-status.component.css'
+})
+export class TodoStatusComponent {
+
+}
